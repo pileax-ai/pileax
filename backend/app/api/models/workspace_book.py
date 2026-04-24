6584415786e96a71b5d5c5941e33d3dcd7e2cb41
pileax <ipileax@gmail.com>
2026-04-24 21:08:05 +0800

    fix(global): 🐛 open book directory

--- backend/app/api/models/workspace_book.py
+++ backend/app/api/models/workspace_book.py
@@ -42,6 +42,7 @@ class WorkspaceBookDetails(WorkspaceBookPublic):
     book_user_id: uuid.UUID
     book_workspace_id: uuid.UUID
     title: str
+    path: str | None = None
     file_url: str | None = None
     cover_url: str | None = None
     author: str | None = None
